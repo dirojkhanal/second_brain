c03b08b85aa1e84c7fd3354a7d69152f5d7a8781
Diroj Khanal <khanaldiroj22@gmail.com>
2026-04-23 19:31:43 +0545

    feat: implement note controller with create, read, update, and list functionalities

---
+++ second-brain-backend/src/controllers/note.controllers.js
@@ -0,0 +1,75 @@
+import * as noteService from '../services/note.services.js';
+import { AppError } from '../utils/appError.js';
+
+//CREATE NOTES 
+
+export const createNote = async (req, res) => {
+    const {title , content} = req.body;
+    const userId = req.user.id;
+
+    const note = await noteService.createNote({
+        userId,
+        title,
+        content
+    });
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            note
+        }
+    })
+};
+
+//get all notes 
+export const getAllNotes = async (req, res) => {
+    const userId = req.user.id;
+
+    const result = await noteService.getAllNotes(userId ,req.query);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            notes :result.notes,
+            pagination: result.pagination
+        }
+    })
+};
+
+//GET SINGLE NOTES 
+export const getNote = async(req, res) => {
+    const {noteId} = req.params;
+    const userId = req.user.id;
+
+    const note = await noteService.getNote(noteId, userId);
+    
+    res.status(200).json({
+        status: 'success',
+        data: {
+            note
+        }
+    })
+
+};
+
+//UPDATE NOTES 
+export const updateNote = async(req, res) => {
+    const {noteId} = req.params;
+    const userId = req.user.id;
+    const {title , content} = req.body;
+
+    const note = await noteService.updateNote({
+        noteId,
+        userId,
+        title,
+        content
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            note
+        }
+
+
+    })
+};
